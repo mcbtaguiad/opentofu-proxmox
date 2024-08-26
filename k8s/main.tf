f45d8168f00de530df6bb773905471f2228bd693
mcbtaguiad <marktaguiad@tagsdev.xyz>
2024-08-27 01:54:47 +0800

    updated main.tf

--- k8s/main.tf
+++ k8s/main.tf
@@ -25,7 +25,7 @@ data "terraform_remote_state" "tagsdev-k8s" {
 #                                    MASTER                                    #
 # ============================================================================ #
 resource "proxmox_vm_qemu" "k8s-master-1" {
-    count = 1
+    count = 3
     name = "k8s-master-${count.index + 1}" 
     desc = "k8s-master-${count.index + 1}" 
     tags = "k8s"
@@ -73,157 +73,9 @@ resource "proxmox_vm_qemu" "k8s-master-1" {
     }
 }
 # ============================================================================ #
-resource "proxmox_vm_qemu" "k8s-master-2" {
-    count = 2
-    name = "k8s-master-${count.index + 1}" 
-    desc = "k8s-master-${count.index + 1}"
-    tags = "k8s"
-    target_node = var.proxmox_host
-    vmid = "11${count.index + 1}"
-
-    clone = var.template_name
-
-    cores   = 2
-    sockets = 1
-    memory  = 2560
-    agent = 1
-    
-    bios = "seabios"
-    scsihw = "virtio-scsi-pci"
-    bootdisk = "scsi0"
-
-    sshkeys = <<EOF
-    ${var.ssh_key}
-    EOF
-
-    os_type   = "cloud-init"
-    cloudinit_cdrom_storage = "tags-nvme-thin-pool1"
-    ipconfig0 = "ip=192.168.254.1${count.index + 1}/24,gw=192.168.254.254"
-
-
-    disks {
-        scsi {
-            scsi0 {
-                disk {
-                    backup = false
-                    size       = 25
-                    storage    = "tags-nvme-thin-pool1"
-                    emulatessd = false
-                }
-            }
-        }
-    }
-
-    network {
-        model = "virtio"
-        bridge = "vmbr0"
-        firewall = true
-        link_down = false
-    }
-}
-# ============================================================================ #
-resource "proxmox_vm_qemu" "k8s-master-3" {
-    count = 3
-    name = "k8s-master-${count.index + 1}" 
-    desc = "k8s-master-${count.index + 1}"
-    tags = "k8s"
-    target_node = var.proxmox_host
-    vmid = "11${count.index + 1}"
-
-    clone = var.template_name
-
-    cores   = 2
-    sockets = 1
-    memory  = 2560
-    agent = 1
-    
-    bios = "seabios"
-    scsihw = "virtio-scsi-pci"
-    bootdisk = "scsi0"
-
-    sshkeys = <<EOF
-    ${var.ssh_key}
-    EOF
-
-    os_type   = "cloud-init"
-    cloudinit_cdrom_storage = "tags-nvme-thin-pool1"
-    ipconfig0 = "ip=192.168.254.1${count.index + 1}/24,gw=192.168.254.254"
-
-
-    disks {
-        scsi {
-            scsi0 {
-                disk {
-                    backup = false
-                    size       = 25
-                    storage    = "tags-nvme-thin-pool1"
-                    emulatessd = false
-                }
-            }
-        }
-    }
-
-    network {
-        model = "virtio"
-        bridge = "vmbr0"
-        firewall = true
-        link_down = false
-    }
-}
-
-# ============================================================================ #
 #                                    WORKER                                    #
 # ============================================================================ #
-resource "proxmox_vm_qemu" "k8s-worker-1" {
-    count = 1
-    name = "k8s-worker-${count.index + 1}" 
-    desc = "k8s-worker-${count.index + 1}"
-    tags = "k8s"
-    target_node = var.proxmox_host
-    vmid = "12${count.index + 1}"
-
-    clone = var.template_name
-
-    cores   = 2
-    sockets = 1
-    memory  = 2048
-    agent = 1
-    
-    bios = "seabios"
-    scsihw = "virtio-scsi-pci"
-    bootdisk = "scsi0"
-
-    sshkeys = <<EOF
-    ${var.ssh_key}
-    EOF
-
-    os_type   = "cloud-init"
-    cloudinit_cdrom_storage = "tags-nvme-thin-pool1"
-    ipconfig0 = "ip=192.168.254.2${count.index + 1}/24,gw=192.168.254.254"
-
-
-    disks {
-        scsi {
-            scsi0 {
-                disk {
-                    backup = false
-                    size       = 25
-                    storage    = "tags-nvme-thin-pool1"
-                    emulatessd = false
-                }
-            }
-        }
-    }
-
-    network {
-        model = "virtio"
-        bridge = "vmbr0"
-        firewall = true
-        link_down = false
-    }
-}
-# ============================================================================ #
-resource "proxmox_vm_qemu" "k8s-worker-2" {
+resource "proxmox_vm_qemu" "k8s-worker" {
     count = 2
     name = "k8s-worker-${count.index + 1}" 
     desc = "k8s-worker-${count.index + 1}"
@@ -233,7 +85,7 @@ resource "proxmox_vm_qemu" "k8s-worker-2" {
 
     clone = var.template_name
 
-    cores   = 2
+    cores   = 4
     sockets = 1
     memory  = 4096
     agent = 1
@@ -272,58 +124,9 @@ resource "proxmox_vm_qemu" "k8s-worker-2" {
     }
 }
 # ============================================================================ #
-resource "proxmox_vm_qemu" "k8s-worker-3" {
-    count = 3
-    name = "k8s-worker-${count.index + 1}" 
-    desc = "k8s-worker-${count.index + 1}"
-    tags = "k8s"
-    target_node = var.proxmox_host
-    vmid = "12${count.index + 1}"
-
-    clone = var.template_name
-
-    cores   = 4
-    sockets = 1
-    memory  = 6144
-    agent = 1
-    
-    bios = "seabios"
-    scsihw = "virtio-scsi-pci"
-    bootdisk = "scsi0"
-
-    sshkeys = <<EOF
-    ${var.ssh_key}
-    EOF
-
-    os_type   = "cloud-init"
-    cloudinit_cdrom_storage = "tags-nvme-thin-pool1"
-    ipconfig0 = "ip=192.168.254.2${count.index + 1}/24,gw=192.168.254.254"
-
-
-    disks {
-        scsi {
-            scsi0 {
-                disk {
-                    backup = false
-                    size       = 25
-                    storage    = "tags-nvme-thin-pool1"
-                    emulatessd = false
-                }
-            }
-        }
-    }
-
-    network {
-        model = "virtio"
-        bridge = "vmbr0"
-        firewall = true
-        link_down = false
-    }
-}
-# ============================================================================ #
 #                                   STORAGE                                    #
 # ============================================================================ #
-resource "proxmox_vm_qemu" "k8s-storage-1" {
+resource "proxmox_vm_qemu" "k8s-storage" {
     count = 1
     name = "k8s-storage-${count.index + 1}" 
     desc = "k8s-storage-${count.index + 1}"
@@ -333,9 +136,9 @@ resource "proxmox_vm_qemu" "k8s-storage-1" {
 
     clone = var.template_name
 
-    cores   = 1
+    cores   = 8
     sockets = 1
-    memory  = 2048
+    memory  = 8192
     agent = 1
     
     bios = "seabios"
@@ -369,56 +172,7 @@ resource "proxmox_vm_qemu" "k8s-storage-1" {
                     emulatessd = false
                 }
             }
-        }
-    }
-
-    network {
-        model = "virtio"
-        bridge = "vmbr0"
-        firewall = true
-        link_down = false
-    }
-}
-# ============================================================================ #
-resource "proxmox_vm_qemu" "k8s-storage-2" {
-    count = 2
-    name = "k8s-storage-${count.index + 1}" 
-    desc = "k8s-storage-${count.index + 1}"
-    tags = "k8s"
-    target_node = var.proxmox_host
-    vmid = "13${count.index + 1}"
-
-    clone = var.template_name
-
-    cores   = 1
-    sockets = 1
-    memory  = 2048
-    agent = 1
-    
-    bios = "seabios"
-    scsihw = "virtio-scsi-pci"
-    bootdisk = "scsi0"
-
-    sshkeys = <<EOF
-    ${var.ssh_key}
-    EOF
-
-    os_type   = "cloud-init"
-    cloudinit_cdrom_storage = "tags-nvme-thin-pool1"
-    ipconfig0 = "ip=192.168.254.3${count.index + 1}/24,gw=192.168.254.254"
-
-
-    disks {
-        scsi {
-            scsi0 {
-                disk {
-                    backup = false
-                    size       = 25
-                    storage    = "tags-nvme-thin-pool1"
-                    emulatessd = false
-                }
-            }
-            scsi1 {
+            scsi2 {
                 disk {
                     backup = false
                     size       = 512
@@ -426,6 +180,7 @@ resource "proxmox_vm_qemu" "k8s-storage-2" {
                     emulatessd = false
                 }
             }
+            
         }
     }
 
